f00096c5478250fd6ab321942eb494703d749de0
dipesh-banknovo <djmalkar840@gmail.com>
2024-06-21 23:57:18 +0530

    tabs for Details added

--- app/src/main/java/com/dipesh/mininetflix/screens/detail/movie/MovieDetailScreen.kt
+++ app/src/main/java/com/dipesh/mininetflix/screens/detail/movie/MovieDetailScreen.kt
@@ -1,5 +1,6 @@
 package com.dipesh.mininetflix.screens.detail.movie
 
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
 import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Arrangement
@@ -9,7 +10,10 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredHeight
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
@@ -21,10 +25,19 @@ import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.ElevatedButton
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults
 import androidx.compose.material3.Text
 import androidx.compose.material3.VerticalDivider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -154,6 +167,39 @@ fun MovieDetailScreen() {
             fontSize = 16.sp,
             color = Color.White,
         )
+
+        var tabIndex by remember { mutableIntStateOf(0) }
+
+        val tabs = listOf("Episodes", "More like this", "Cast & more")
+
+        TabRow(
+            selectedTabIndex = tabIndex,
+            containerColor = Color.Black,
+            contentColor = Color.White,
+            modifier = Modifier
+                .background(Color.Black)
+                .padding(vertical = 8.dp)
+                .fillMaxWidth()
+        ) {
+            tabs.forEachIndexed { index, title ->
+                Tab(
+                    selected = tabIndex == index,
+                    onClick = { tabIndex = index },
+                    content = {
+                        Text(
+                            text = title,
+                            fontSize = 16.sp
+                        )
+                    }
+                )
+            }
+        }
+
+        when (tabIndex) {
+            0 -> EpisodesTab()
+            1 -> MoreTab()
+            2 -> CastTab()
+        }
     }
 }
 
@@ -236,5 +282,41 @@ private fun MovieTopGenres() {
 @Composable
 @Preview
 fun PreviewMovieTopGenres() {
-    MovieTopGenres()
+
+}
+
+@Composable
+fun EpisodesTab() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .requiredHeight(200.dp)
+            .background(Color.Green)
+    ) {
+
+    }
+}
+
+@Composable
+fun MoreTab() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .requiredHeight(200.dp)
+            .background(Color.Red)
+    ) {
+
+    }
+}
+
+@Composable
+fun CastTab() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .requiredHeight(200.dp)
+            .background(Color.Yellow)
+    ) {
+
+    }
 }
